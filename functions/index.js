1852e651a48e1d8568dc7026242524570f286880
GreanDev <greanestofbeans@protonmail.com>
2024-11-27 07:26:52 -0500

    Revamp IPOG

---
+++ functions/index.js
@@ -0,0 +1,28 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+const {
+  onDocumentWritten,
+  onDocumentCreated,
+} = require("firebase-functions/v2/firestore");
+const { onRequest } = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+exports.added = onDocumentCreated("chat1/{msg}", (event) => {
+  const snapshot = event.data;
+  if (!snapshot) {
+    console.log("No data associated with the event");
+    return;
+  }
+  const data = snapshot.data();
+
+  console.log(data.content);
+});
